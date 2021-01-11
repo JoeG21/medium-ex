517a6a6b4c6f9b8083d95cf79966403f2111ea5d
JoeG21 <jcggomez@live.com>
2021-01-10 20:43:56 -0600

    finish

--- react-hooks-useState/src/App.js
+++ react-hooks-useState/src/App.js
@@ -1,19 +1,35 @@
 import { useState } from 'react';
+// import React, { Component } from 'react';
 import './App.css';
 
 function App() {
-  const [color, setColor] = useState('?')
- 
+  // const [state, setState] = useState({ color: '?', phrase: 'Hello World' })
+  // const color = state.color
+  // const phrase = state.phrase
+  // Using Objects in your state ⬆️
+
+
+  // const [color, setColor] = useState('?')
+  // Hardcoded Version ⬆️
+  // Will run multiple times when you update the state
+
+
+  const [color, setColor] = useState(() => {
+    return '?'
+  })
+  // Functional Version ⬆️
+  // Will only run one time when the component renders
+
   const handleRed = () => {
-    setColor(prevColor => <div className='card-red'> Red </div>)
+    setColor(prevState => <div className='card-red'> Red </div>)
   }
 
   const handleGreen = () => {
-    setColor(prevColor => <div className='card-green'> Green </div>)
+    setColor(prevState => <div className='card-green'> Green </div>)
   }
 
   const handleBlue = () => {
-    setColor(prevColor => <div className='card-blue'> Blue </div>)
+    setColor(prevState => <div className='card-blue'> Blue </div>)
   }
 
   return (
@@ -29,5 +45,47 @@ function App() {
     </div>
   );
 }
+
+// Class Component Version ⬇️
+// class App extends Component {
+
+//   constructor() {
+//     super();
+//     this.state = {color: '?'}
+//   }
+
+//   handleRed = () => {
+//     this.setState({
+//       color: <div className='card-red'> Red </div>
+//     })
+//   }
+
+//   handleGreen = () => {
+//     this.setState({
+//       color: <div className='card-green'> Green </div>
+//     })
+//   }
+
+//   handleBlue = () => {
+//     this.setState({
+//       color: <div className='card-blue'> Blue </div>
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div className="App-header">
+//         <div className='button-boxs'>
+//           <button className='button-red' onClick={this.handleRed}> Red </button>
+//           <button className='button-green' onClick={this.handleGreen}> Greeen </button>
+//           <button className='button-blue' onClick={this.handleBlue}> Blue </button>
+//         </div>
+//         <div className='card'>
+//           <span> {this.state.color} </span>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
 
 export default App;
