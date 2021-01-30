578b43729e57e8bcee939e9763dfc7fd6dcfc193
JoeG21 <jcggomez@live.com>
2021-01-29 22:03:26 -0600

    working

--- react-searchbar/src/App.js
+++ react-searchbar/src/App.js
@@ -1,24 +1,30 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Search from './Search'
+import MainContainer from './MainContainer'
 
 const App = () => {
+  const [users, setUsers] = useState([])
   const [searchTerm, setSearchTerm] = useState("")
   const [foundSearch, setFoundSearch] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(json => setUsers(json));
+  }, [])
 
   const handleSearch = () => {
     setSearchTerm(() => {
       console.log('hello')
     })
-  }
+  };
 
   return (
     <div className="App">
       <Search handleSearch={handleSearch} />
-      <ul>
-        1 2 3
-      </ul>
+      <MainContainer users={users} />
     </div>
   );
 }
