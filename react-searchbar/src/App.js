5c2f60bfa19b1ac693d7ba81c1a58cfeeb0594cc
JoeG21 <jcggomez@live.com>
2021-01-31 17:52:34 -0600

    bug no more

--- react-searchbar/src/App.js
+++ react-searchbar/src/App.js
@@ -11,10 +11,17 @@ const App = () => {
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(json => setUsers(json));
+      .then(res => res.json())
+      .then(json => setUsers(json));
   }, [])
 
+  const searchUser = (e) => {
+    setSearchTerm(prevState => e.target.value)
+
+    // setSearchTerm(prevState => console.log(e.target.value))
+  };
+
+  // // WORKING CODE ⬇️
   useEffect(() => {
     setFoundUsers(
       users.filter( user => {
@@ -22,18 +29,35 @@ const App = () => {
     ))
   }, [searchTerm, users])
 
-  const searchUser = (e) => {
-    setSearchTerm(prevState => e.target.value)
-  };
+  // useEffect(() => {
+  //   setFoundUsers(
+  //     let current = users.filter(user => {
+  //       user.name.toLowerCase().includes(searchTerm.toLowerCase())
+  //       if( current !== users) {
+  //         console.log("hello")
+  //       } else {
+  //         return foundUsers(current)
+  //       }
+  //     }
+  //     ))
+  // }, [searchTerm, users])
 
   return (
     <div className="App">
       <Search searchUser={searchUser} />
       <br />
-      {foundUsers.length === 0 ? 
-        <UserContainer users={users} /> 
-        : 
-        <UserContainer users={foundUsers} />}
+      {foundUsers.length !== 0 ?
+      <UserContainer users={foundUsers} />
+      : foundUsers.length === 0 ?
+      <h1>No</h1>
+      // <UserContainer users={} />
+      :
+      <UserContainer users={users} />}
+
+      {/* {foundUsers.length === 0 ?
+        <UserContainer users={users} />
+        :
+        <UserContainer users={foundUsers} />} */}
     </div>
   );
 }
