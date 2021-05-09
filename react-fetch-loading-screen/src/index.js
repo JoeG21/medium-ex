784ed94ae5d5f7762092fc27bd16109f4aa63242
JoeG21 <jcggomez@live.com>
2021-05-09 14:59:33 -0500

    starting

--- react-fetch-loading-screen/src/index.js
+++ react-fetch-loading-screen/src/index.js
@@ -4,21 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-
-const reducer = (state = {}) => {
-  return state
-}
-
-const store = createStore(
-  reducer
-)
-
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
--- walkthrough-react-react/src/index.js
+++ walkthrough-react-react/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 const reducer = (state = {}) => {
