c3f9546ee167ed7d2a8a90ee1305d92ce54f445a
JoeG21 <jcggomez@live.com>
2021-05-09 20:50:33 -0500

    finish

--- react-fetch-loading-screen/src/App.js
+++ react-fetch-loading-screen/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(setLoading(true))
+    .then(res => res.json())
+    .then(users => setTimeout(() => handleUsers(users), 3000))
+  }, [])
+
+  const handleUsers = (users) => {
+    setUsers(users)
+    setLoading(false)
+  }
+
+  let startLoading = (
+    <h1> LOADING </h1>
+  )
+
+  let userContainer = (
+    <>
+      {users.map(user => <ul><p> {user.name} </p></ul>)}
+    </>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? startLoading : userContainer }
+
+      {/* {loading ? console.log('this is true') : console.log('this is false')} */}
     </div>
   );
 }
